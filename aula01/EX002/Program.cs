d56a7af7cc48ebc846bbbacc9e256256c2d339b6
alexandre dias <xanderio@outlook.com>
2024-08-09 19:29:18 -0300

    Organizando pastas

---
+++ aula01/EX002/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um número inteiro: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int soma = 0;
+
+        for (int i = 1; i <= N; i++)
+        {
+            soma += i;
+        }
+
+        Console.WriteLine($"A soma de todos os números de 1 até {N} é: {soma}");
+    }
+}
+Console.ReadKey();
